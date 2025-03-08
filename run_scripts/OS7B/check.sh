e99fa14573297b5eea0706f9190889deb31e6c42
ZZZhr-1 <1103084704@qq.com>
2025-03-08 20:01:47 +0800

    os7b

---
+++ run_scripts/OS7B/check.sh
@@ -0,0 +1,6 @@
+python OS7B/check_attack.py \
+    --model_path OS-Copilot/OS-Atlas-Base-7B \
+    --screenspot_imgs output/ \
+    --screenspot_test datas \
+    --task all \
+    --max_pixels 1024
--- run_scripts/Uground/check.sh
+++ run_scripts/Uground/check.sh
@@ -1,6 +1,6 @@
 python Uground/check_attack.py \
     --model_path osunlp/UGround-V1-7B \
-    --screenspot_imgs datas/screenspotv2_image \
+    --screenspot_imgs output \
     --screenspot_test datas \
     --task all \
     --max_pixels 1024
